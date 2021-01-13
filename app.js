f8642d5a5164b1d007e508a1ad533d4368d2b7b9
habib610 <habib54562@gmail.com>
2021-01-13 16:25:04 +0600

    second-commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var num = 12;
+num = 56;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ app.ts
@@ -0,0 +1,9 @@
+var num = 12;
+
+num = 56 ;
+
+const circ = (diameter: number) => {
+    return diameter *  Math.PI
+}
+
+console.log(circ(7.5))
